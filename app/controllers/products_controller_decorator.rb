1a70fd74b60bb237857916df61edaa70793a70b6
David Ruyer <david.ruyer@gmail.com>
2011-05-24 16:28:14 +0200

    Fix

--- app/controllers/products_controller_decorator.rb
+++ app/controllers/products_controller_decorator.rb
@@ -3,12 +3,5 @@ ProductsController.class_eval do
   Taxonomy.all.each do |t|
     has_scope t.filter_name
   end
-  
-  # TODO : directly customize searcher
-  before_filter :get_filtered_products, :only => :index
-
-  def get_filtered_products
-    @filtered_products = apply_scopes(Product).all
-  end
-    
+      
 end
